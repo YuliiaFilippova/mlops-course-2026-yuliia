3492b6d465b85cc1c7dda186fadd2ffc39ab0cd2
Yuliia Filippova <yuliiafilippova@Yuliias-MacBook-Pro.local>
2026-06-16 20:19:13 +0200

    fixing errors

--- mlops-course-05/terraform/s3_buckets.tf
+++ mlops-course-05/terraform/s3_buckets.tf
@@ -3,6 +3,6 @@ module "s3_bucket" {
   for_each = { for s3 in var.s3_buckets : s3.key => s3 }
   source   = "./modules/s3-bucket"
 
-  bucket = each.value.key
-  tags   = each.value.tags
+  bucket = join(var.delimiter, [each.value.key, var.environment])
+  tags   = merge(try(each.value.tags, {}), { environment = var.environment })
 }
